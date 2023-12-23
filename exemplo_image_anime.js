8890abde24e9f8dbe8f690a611782cf95795b771
Markos vips <78027781+TioMarkZ@users.noreply.github.com>
2023-12-23 18:13:24 -0300

    Update exemplo_image_anime.js

--- exemplo_image_anime.js
+++ exemplo_image_anime.js
@@ -2,13 +2,16 @@ import fetch from 'node-fetch';
 import fs from 'fs/promises';
 import axios from 'axios';
 import path from 'path';
-let image_num = 200 // número de imagens que deseja
+
+let image_num = 300 // número de imagens que deseja
 
 async function fetchDataAndSaveUrls(numFetches) {
     const urls = [];
+    const url2 = [];
+    const downloadErrors = [];
   
     for (let i = 0; i < numFetches; i++) {
-      const response = await fetch('https://api.waifu.pics/sfw/lick');
+      const response = await fetch('https://api.waifu.pics/sfw/hug');
       const data = await response.json();
       console.log(data.url);
       urls.push(data.url);
@@ -19,40 +22,43 @@ async function fetchDataAndSaveUrls(numFetches) {
     await fs.writeFile('urls.json', jsonData);
     console.log('as urls foram salvas no arquivo urls.json');
   
-    await downloadGifsFromUrls(urls);
-  }
+    await downloadGifsFromUrls(urls, url2, downloadErrors);
   
-  async function downloadGifsFromUrls(urls) {
+    const jsonData2 = JSON.stringify( {urls: url2}, null, 2);
+    await fs.writeFile('url2.json', jsonData2);
+    console.log('arquivo url2.json foi gerado');
+  
+    if (downloadErrors.length > 0) {
+      console.log(`Os seguintes gifs não foram baixados com sucesso: ${downloadErrors.join(', ')}`);
+    }
+}
+
+async function downloadGifsFromUrls(urls, url2, downloadErrors) {
     const currentWorkingDirectory = process.cwd();
+    const downloadedUrls = new Set();
   
     const downloadPromises = urls.map(async (url, index) => {
       try {
         const response = await axios.get(url, { responseType: 'arraybuffer' });
-        const gifFileName = `lick${(index + 1).toString().padStart(2, '0')}.gif`;
+        const gifFileName = `hug${(index + 1).toString().padStart(2, '0')}.gif`;
         const filePath = path.join(currentWorkingDirectory, gifFileName);
   
         await fs.writeFile(filePath, response.data, 'binary');
         console.log(`Downloaded: ${gifFileName}`);
+  
+        if (downloadedUrls.has(url)) {
+          console.log(`O gif ${gifFileName} é repetido`);
+        } else {
+          downloadedUrls.add(url);
+          url2.push(url);
+        }
       } catch (error) {
         console.error(`Erro ao baixar o gif ${index + 1}: ${error.message}`);
+        downloadErrors.push(index + 1);
       }
     });
   
     await Promise.all(downloadPromises);
-  }
-  
-fetchDataAndSaveUrls(image_num) 
-
-
-  const url2 = [];
-
-  for (let i = 1; i <= image_num; i++) {
-    let paddedNumber = String(i).padStart(3, '0');
-    url2.push(`https://raw.githubusercontent.com/HACKERESOBERANO/bandeiras/main/slap/slap${paddedNumber}.gif`);
 }
 
-
-  const jsonData = JSON.stringify( {urls: url2}, null, 2);
-
-  await fs.writeFile('url2.json', jsonData);
-  console.log('arquivo url2.json foi gerado');
+fetchDataAndSaveUrls(image_num);
