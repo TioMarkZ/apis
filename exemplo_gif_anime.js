6b814b14ac0f50e76d8866c035e64ff86dc35e96
Markos vips <78027781+TioMarkZ@users.noreply.github.com>
2023-12-24 06:32:07 -0300

    ☕ deseja baixar em massa?

--- exemplo_gif_anime.js
+++ exemplo_gif_anime.js
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 import axios from 'axios';
 import path from 'path';
 
-let image_num = 500 // número de imagens que deseja
+let image_num = 50 // número de gifs que deseja
 
 async function fetchDataAndSaveUrls(numFetches) {
     const urls = [];
